4f53e64753995e7ef2c51ec26b3d1a4674a4e8fd
edenbtran <edenbtran@gmail.com>
2024-07-19 17:01:28 -0500

    change for heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://peaceful-lowlands-82638-63eb71fcbc7e.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
